1954b4adca83f473185fcccddec7f5a99ae07ac0
JellyBeans2011 <savagepervert999@gmail.com>
2023-05-26 22:39:57 +0300

    lol

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
+// EXPORTS
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded());
 
+// GROCERYLIST
+const groceryList = [
+    {
+        item: 'milk',
+        quantity: 2
+    },
+    {
+        item: 'cereal',
+        quantity: 2
+    },
+    {
+        item: 'lettace',
+        quantity: 2
+    },
+];
 
+// GET REQUESTS
+app.get('/groceries', (req, res) => {
+    res.send(groceryList);
+});
+
+// POST REQUESTS
+app.post('/groceries', (req, res) => {
+    console.log(req.body);
+    groceryList.push(req.body);
+    res.send(201);
+});
+
+// LISTENING TO A PORT
 app.listen(PORT, () => console.log(`Running a server on ${PORT}...`));
