a85c4de967e46a6104aa67b16af2dba1bea74e00
JellyBeans2011 <savagepervert999@gmail.com>
2023-05-31 22:33:42 +0300

    updated

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,31 +3,30 @@ const router = Router();
 const User = require('../database/schemas/User');
 const { hashPassword, comparePasswords } = require('../utils/helpers');
 
-router.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    if (username && password) {
-        if (req.session.user) {
-            res.send(req.session.user);
-        } else {
-            req.session.user = {
-                username,
-            }
-        };
-        res.send(req.session);
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) return res.status(400);
+    const userDB = await User.findOne({ email });
+    if (!userDB) return res.send(401);
+    const isValid = comparePasswords(password, userDB.password);
+    if (isValid) {
+        console.log('Authenticated Successfully!');
+        req.session.user = userDB;
+        return res.send(200);
     } else {
-        res.send(401);
+        console.log('Failed to Authenticate');
+        return res.send(401);
     }
 });
 
 router.post('/register', async (req, res) => {
-    const { username, email } = req.body;
-    const userDB = await User.findOne({ $or: [{ username }, { email }] });
+    const { email } = req.body;
+    const userDB = await User.findOne({ $or: [{ email }] });
     if (userDB) {
         res.status(400).send({ msg: 'User already exists!' });
     } else {
         const password = hashPassword(req.body.password);
-        console.log(password);
-        const newUser = await User.create({ username, password, email });
+        const newUser = await User.create({ password, email });
         res.send(201);
     }
 })
