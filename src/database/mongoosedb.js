caad2836cefb66cd320af92730cd736fd431cae3
JellyBeans2011 <savagepervert999@gmail.com>
2023-06-01 10:29:51 +0300

    -

--- src/database/mongoosedb.js
+++ src/database/mongoosedb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://savage:(password)@cluster0.4dcstto.mongodb.net/users_data')
+mongoose.connect('mongodb+srv://savage:<password>@cluster0.4dcstto.mongodb.net/users_data')
     .then(() => console.log('Connected to DB'))
     .catch((err) => console.log(err))
